07a995712a61ac02000053eec5c6fafcae5f0b7f
Jiří Smejkal <jiri.smejkal5@seznam.cz>
2023-11-17 13:44:22 +0100

    added user to category and expense model

---
+++ prisma/migrations/20231117123305_adjust_category_user_relation/migration.sql
@@ -0,0 +1,91 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Category` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userId` to the `Expense` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL,
+    CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" DATETIME,
+    "image" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Category" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Category_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Category" ("description", "id", "name") SELECT "description", "id", "name" FROM "Category";
+DROP TABLE "Category";
+ALTER TABLE "new_Category" RENAME TO "Category";
+CREATE TABLE "new_Expense" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "amount" REAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    CONSTRAINT "Expense_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Expense_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Expense" ("amount", "categoryId", "description", "id", "name") SELECT "amount", "categoryId", "description", "id", "name" FROM "Expense";
+DROP TABLE "Expense";
+ALTER TABLE "new_Expense" RENAME TO "Expense";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,16 +1,27 @@
 import { categoryFormSchema } from "@/app/categories/category";
 import { db } from "@/server/db";
 import { z } from "zod";
+import { getServerAuthSession } from "@/server/auth";
 
 export const POST = async (request: Request) => {
   try {
     const bodyJson = await request.json();
     const parsedCategory = categoryFormSchema.parse(bodyJson);
+    const status = await getServerAuthSession();
+    const userId = status?.user.id;
+    console.log('userIdxxxxxxxxxxxxxxxxxxxxxxxxxx', status)
+
+    if (!userId) {
+      return new Response("Server error - could not find userId", {
+        status: 500,
+      });
+    }
 
     const newCategory = await db.category.create({
       data: {
         name: parsedCategory.name,
         description: parsedCategory.description,
+        userId: userId,
       },
     });
 
