e5e0a6f5eb16f59241218e77ebe8baf46b5e1577
Tomas Kadavy <xkadavy@fi.muni.cz>
2023-11-19 13:14:57 +0100

    responsive layout

---
+++ src/app/BurgerButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import Menu from "./Menu";
+
+const BurgerButton = () => {
+  const [menuToggled, setMenuToggled] = useState<boolean>(false);
+  const toggleMenu = () => {
+    setMenuToggled(!menuToggled);
+  };
+
+  return (
+    <div>
+      <button className="h-12 w-full" onClick={toggleMenu}>
+        ☰ Show menu
+      </button>
+      {menuToggled && (
+        <div className="w-screen">
+          <Menu />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default BurgerButton;
