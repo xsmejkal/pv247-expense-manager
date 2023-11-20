59a98c80d394be07ffbe765bdc1ea23a4241a995
Tomas Kadavy <xkadavy@fi.muni.cz>
2023-11-20 15:59:16 +0100

    header same height all the time

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,10 +37,12 @@ export default async function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className={`${inter.className} flex flex-col overflow-x-hidden bg-gray-00`}>
+      <body
+        className={`${inter.className} flex flex-col overflow-x-hidden bg-gray-00`}
+      >
         <Providers>
-          <div className="bg-gray-100 w-full px-5 py-2 flex justify-between items-center fixed top-0 z-20">
-          <Link className="hover:text-blue-600" href="/">
+          <div className="bg-gray-100 h-16 w-full px-5 py-2 flex justify-between items-center fixed top-0 z-20">
+            <Link className="hover:text-blue-600" href="/">
               Home
             </Link>
             <LoginStatus />
@@ -52,16 +54,12 @@ export default async function RootLayout({
               <Menu />
             </div>
 
-            <div className="flex-1 overflow-y-auto pl-60 pt-2">
-              {children}
-            </div>
+            <div className="flex-1 overflow-y-auto pl-60 pt-2">{children}</div>
           </div>
 
           <div className="md:hidden">
             <BurgerButton />
-            <div className="overflow-y-auto pt-16">
-              {children}
-            </div>
+            <div className="overflow-y-auto pt-16">{children}</div>
           </div>
         </Providers>
       </body>
