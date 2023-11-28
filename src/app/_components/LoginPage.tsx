ceca459d70873f43e3cd619400e1534bdafbf637
Tomas Kadavy <xkadavy@fi.muni.cz>
2023-11-28 22:02:00 +0100

    fix login page

--- src/app/_components/LoginPage.tsx
+++ src/app/_components/LoginPage.tsx
@@ -7,20 +7,10 @@ const LoginPage = () => {
     <div className="flex h-screen justify-center items-center bg-gray-900">
       <div className="flex flex-col items-center">
         {" "}
-        <h1 className="text-4xl font-bold text-white mb-4">
+        <h1 className="text-4xl font-bold text-white mb-8">
           {" "}
           Expense Manager++
         </h1>
-        <div className="relative mb-4 w-96 h-96">
-          {" "}
-          <Image
-            src="/logo.png"
-            alt="Expense Manager++ Logo"
-            layout="fill"
-            objectFit="contain"
-            priority
-          />
-        </div>
         <button
           onClick={() => signIn("discord")}
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-6 rounded-full focus:outline-none"
